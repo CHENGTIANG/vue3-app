eae0eb857312f6567d83910d5a359c527cd53b96
Grant Chan <grantchan@metaarchit.com>
2022-10-02 11:31:31 +0800

    feat: add me page content

--- src/pages/home/MePage.vue
+++ src/pages/home/MePage.vue
@@ -1,3 +1,22 @@
 <template>
-  <div class="pa-2">Me</div>
+  <div>
+    <v-list class="mb-2">
+      <v-list-item prepend-avatar="https://picsum.photos/200/200" title="Grant">
+      </v-list-item>
+
+      <v-list-item prepend-icon="mdi-image">
+        <v-list-item-title>Moments</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item prepend-icon="mdi-cog-outline">
+        <v-list-item-title>Settings</v-list-item-title>
+      </v-list-item>
+      <v-list-item prepend-icon="mdi-information">
+        <v-list-item-title>About</v-list-item-title>
+      </v-list-item>
+    </v-list>
+    <div class="pa-2">
+      <v-btn block>Sign Out</v-btn>
+    </div>
+  </div>
 </template>
