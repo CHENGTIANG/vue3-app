f7ec58cd591d71d39944b3fdb55b32f9fb97a052
Grant Chan <grantchan@metaarchit.com>
2022-11-19 11:50:24 +0800

    feat: add page content

--- src/pages/home/FavoritesPage.vue
+++ src/pages/home/FavoritesPage.vue
@@ -1,3 +1,70 @@
 <template>
-  <div class="pa-2">Favorites</div>
+  <v-container fluid>
+    <v-row dense>
+      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+        <v-card>
+          <v-img
+            :src="card.src"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+            cover
+          >
+            <v-card-title class="text-white" v-text="card.title"></v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              size="small"
+              color="surface-variant"
+              variant="text"
+              icon="mdi-heart"
+            ></v-btn>
+
+            <v-btn
+              size="small"
+              color="surface-variant"
+              variant="text"
+              icon="mdi-bookmark"
+            ></v-btn>
+
+            <v-btn
+              size="small"
+              color="surface-variant"
+              variant="text"
+              icon="mdi-share-variant"
+            ></v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          title: "Pre-fab homes",
+          src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+          flex: 6,
+        },
+        {
+          title: "Favorite road trips",
+          src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+          flex: 6,
+        },
+        {
+          title: "Best airlines",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 6,
+        },
+      ],
+    };
+  },
+};
+</script>
